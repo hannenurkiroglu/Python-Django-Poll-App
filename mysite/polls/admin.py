9febfbd7788e89e3b68822640991474b6a8c2ad6
Hannenur Kıroğlu <hannenur.yazbahar@bil.omu.edu.tr>
2021-07-07 09:45:59 +0300

    FİX: Fix the admin page error

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Choice, Choise, Question
+from .models import Choice, Question
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -15,6 +15,6 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display  = ('question_text', 'pub_date', 'was_published_recently')
     list_filter   = ['pub_date']
     search_fields = ['question_text']
-    
+
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
